0ee7093d17c896ba8b7d2b9fb4b5251a6482e702
Ruben Mariscal <mariscal.rmc@gmail.com>
2016-11-05 17:21:50 +0100

    Se modifica la classe main

--- src/hello.java
+++ src/hello.java
@@ -3,6 +3,9 @@
  */
 public class hello {
     public static void main(String[] args) {
+        printHola();
+    }
+    public static void printHola(){
         System.out.println("Hola món!");
     }
 }
